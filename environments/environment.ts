dcc687ba4dab480962269243900170344c0f5362
abderahimali <83404924+abderahimali@users.noreply.github.com>
2022-06-01 05:48:54 +0100

    Add files via upload
    ajout des composants (forgot-password, sign-in sign-up, verifiy-email) qui permettent la connexion a firebase

---
+++ environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+   firebase : {
+    apiKey: "AIzaSyAMW5DDhfbtJTni08_pX1w_x3iLAMy-pxM",
+    authDomain: "cv-ict-202-with-angular.firebaseapp.com",
+    projectId: "cv-ict-202-with-angular",
+    storageBucket: "cv-ict-202-with-angular.appspot.com",
+    messagingSenderId: "642069097454",
+    appId: "1:642069097454:web:9f0611cb1fbd614b307cff"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
---
+++ services/db.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+import { HttpClient , HttpHeaders } from '@angular/common/http';
+import { Observable , of } from 'rxjs'; 
+import { db } from '../dbStructure'
+import { competance } from '../competanceStructure';
+import { experience } from '../experienceStructure';
+import { biographie } from '../biographieStructure';
+
+export class connectDb{
+
+}
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DbServiceInformation{
+  
+  private Url = 'http://localhost:5000/information';
+  private UrlCompetance = 'http://localhost:5000/competance';
+  private UrlExperiance = 'http://localhost:5000/experience';
+  private UrlBiographie = 'http://localhost:5000/biographie';
+
+  constructor(private http:HttpClient) { }
+
+  getInformationDb(){
+    return this.http.get(this.Url);
+  }
+
+  getInformationCompetance(){
+    return this.http.get(this.UrlCompetance);
+  }
+
+  getExperience(){
+    return this.http.get(this.UrlExperiance);
+  }
+  getBiographie(){
+    return this.http.get(this.UrlBiographie);
+  }
+
+  deleteCompetance(competance:competance){
+    const url = `${this.UrlCompetance}/${competance.id}`;
+    return this.http.delete(url);
+  }
+  
+  deleteExperience(cpt:experience){
+    const url = `${this.UrlExperiance}/${cpt.id}`;
+    return this.http.delete(url);
+  }
+
+  deleteBiographie(bio:biographie){
+    const url = `${this.UrlBiographie}/${bio.id}`;
+    return this.http.delete(url);
+  }
+
+  addSkillService(competance:competance){
+    return this.http.post<competance>(this.UrlCompetance , competance , httpOptions);
+  }
+
+  addExperienceService(cpt:experience){
+    return this.http.post<experience>(this.UrlExperiance , cpt , httpOptions);
+  }
+
+  addBiography(bio:biographie){
+    return this.http.post<biographie>(this.UrlBiographie , bio , httpOptions)
+  }
+}
+
+
